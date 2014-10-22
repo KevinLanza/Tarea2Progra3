95afa1847275ad3e1280c5292e1abbf403437ff7
Alumno <alumno@localhost.localdomain>
2014-10-22 08:19:02 -0600

    Tarea

---
+++ clases.cpp
@@ -0,0 +1,11 @@
+#include "clases.h"
+
+clases::clases()
+{
+    //ctor
+}
+
+clases::~clases()
+{
+    //dtor
+}
---
+++ clases.h
@@ -0,0 +1,14 @@
+#ifndef CLASES_H
+#define CLASES_H
+
+
+class clases
+{
+    public:
+        clases();
+        virtual ~clases();
+    protected:
+    private:
+};
+
+#endif // CLASES_H
